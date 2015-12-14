b090d24987e462843369b9689fc1e007ed881e91
Ethan Hann <ethanhann@gmail.com>
2015-12-13 22:28:27 -0500

    Add nav examples and rename files

--- demo/app.js
+++ demo/app.js
@@ -23,6 +23,20 @@
             iconClass: 'glyphicon-user',
             href: '/user-profile'
         });
+
+        eehMenuProvider
+        .menuItem('bar.menuItem1', {
+            text: 'Menu Item 1',
+            href: '#'
+        })
+        .menuItem('bar.menuItem2', {
+            text: 'Menu Item 2',
+            href: '#'
+        })
+        .menuItem('bar.menuItem3', {
+            text: 'Menu Item 3',
+            href: '#'
+        });
     }]);
 
     angular.module('myApp').controller('appController', function ($scope) {
